d3ab5981a7fb0678182cfde5be4c0089b2265965
Natkamon-Luela <s6519c10014@sau.ac.th>
2025-02-07 15:15:23 +0700

    V.2 Edit backg

---
+++ lib/views/manu_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ManuUI extends StatefulWidget {
+  const ManuUI({super.key});
+
+  @override
+  State<ManuUI> createState() => _ManuUIState();
+}
+
+class _ManuUIState extends State<ManuUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+    );
+  }
+}
